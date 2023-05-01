dc95b0f460f18f456693d02890ea1bda1a32c2e6
carloshmfernandes <carloshmfernandes@gmail.com>
2023-05-01 14:00:01 -0300

    Projeto inicial - Aula 01

--- src/main/java/br/com/fiap/AbcTechApi/controller/HealthCkechController.java
+++ src/main/java/br/com/fiap/AbcTechApi/controller/HealthCkechController.java
@@ -1,7 +1,10 @@
 package br.com.fiap.AbcTechApi.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +17,17 @@ public class HealthCkechController {
     public ResponseEntity<String> status(){
         return ResponseEntity.ok("UP");
     }
+
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() throws IOException{
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+        properties.load(inputStream);
+
+        return ResponseEntity.ok(properties.get("build_name") + " - " + properties.get("build_version"));
+
+        
+    }
+
 }
